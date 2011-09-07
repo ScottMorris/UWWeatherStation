015d8fe586d2babe54ee867e81d9be2e89f6d375
Scott Morris <scott.moris@gmail.com>
2011-09-07 16:11:06 -0500

    Project Created

---
+++ UWWeatherSation/src/ca/scottibmorris/weather/UWWeatherSationActivity.java
@@ -0,0 +1,13 @@
+package ca.scottibmorris.weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UWWeatherSationActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
