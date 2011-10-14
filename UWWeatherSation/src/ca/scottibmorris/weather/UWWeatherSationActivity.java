99f4d806b5e52177f4f455f5907fb04d996205f9
Scott Morris <scott.moris@gmail.com>
2011-10-14 13:02:43 -0500

    Got the UI working.

--- UWWeatherSation/src/ca/scottibmorris/weather/UWWeatherSationActivity.java
+++ UWWeatherSation/src/ca/scottibmorris/weather/UWWeatherSationActivity.java
@@ -2,12 +2,27 @@ package ca.scottibmorris.weather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class UWWeatherSationActivity extends Activity {
-    /** Called when the activity is first created. */
+    /* 
+     * Resource Variables
+     */
+	public ImageView logo;
+    
+	/*
+	 * Other Variables
+	 */
+    String tempSuffix = "°C";
+    
+    
+	/** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        logo = (ImageView)findViewById(R.id.uwLogo);
+        logo.setImageResource(R.drawable.uwaterloo_logo);
     }
 }
